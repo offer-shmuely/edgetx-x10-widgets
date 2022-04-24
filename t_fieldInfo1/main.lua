6a93da600c1306aaff334dd84433320b8e15e544
Shmuely <offer.shmuely@cognyte.com>
2022-04-25 00:39:17 +0300

    updates

--- t_fieldInfo1/main.lua
+++ t_fieldInfo1/main.lua
@@ -1,91 +1,55 @@
--- test_getFieldInfo
+local UNIT_ID_TO_STRING = { "V", "A", "mA", "kts", "m/s", "f/s", "km/h", "km/h", "mph", "m", "m", "f", "°C", "°C", "°F", "%", "mAh", "W", "mW", "dB", "rpms", "g", "°", "Rad" }
 
-local UNIT_ID_TO_STRING = { "V", "A", "mA", "kts", "m/s", "f/s", "km/h", "mph", "m", "f", "°C", "°F", "%", "mAh", "W", "mW", "dB", "rpm", "g", "°", "rad", "ml", "fOz", "ml/m", "Hz", "uS", "km" }
+--------------------------------------------------------------
+local function log(s)
+  return;
+  --print("Batt_A1: " .. s)
+end
+--------------------------------------------------------------
 
 local options = {
-  { "Sensor", SOURCE, 0 },
-  { "Color", COLOR, WHITE },
-  { "Shadow", BOOL, 0 },
-  { "LowestCell", BOOL, 1 }
+  { "Source1", SOURCE, 251 }, -- RSSI
+  { "Source2", SOURCE, 254 }, -- RxBt
+  { "Source3", SOURCE, 1 }    -- Ail
 }
 
--- workaround for companion simulator issues
-local function isWidgetInvalid(wgt)
-
-  if (wgt == nil) then return false end
-  if type(wgt) ~= "table" then return false end
-  if (wgt.options == nil) then return false end
-  if type(wgt.options) ~= "table" then return false end
-  if (wgt.zone == nil) then return false end
-  if type(wgt.zone) ~= "table" then return false end
-  --print("aaaaa create::wgt=" .. type(wgt))
-  --print("aaaaa create::options=" .. type(options))
-end
-
-
--- This function is runned once at the creation of the widget
 local function create(zone, options)
   local wgt = {
-    zone = nil,
-    options = nil,
-    counter = 0
+    zone = zone,
+    options = options,
   }
-
-  -- workaround for companion simulator issues
-  print("aaaaa create::wgt=" .. type(wgt))
-  print("aaaaa create::options=" .. type(options))
-
-  if type(wgt) ~= "table" then
-    return wgt
-  end
-  --if type(options) ~= "table" then
-  --  return wgt
-  --end
-
-  wgt.zone = zone
-  wgt.options = options
-
-  -- use default if user did not set, So widget is operational on "select widget"
-  if wgt.options.Sensor == 0 then
-    wgt.options.Sensor = "Cels"
-  end
-
-  wgt.options.LowestCell = wgt.options.LowestCell % 2 -- modulo due to bug that cause the value to be other than 0|1
 
   return wgt
 end
 
--- This function allow updates when you change widgets settings
 local function update(wgt, options)
-  if (isWidgetInvalid(wgt)) then return end
-
+  if (wgt == nil) then return end
   wgt.options = options
 end
 
 local function showField(wgt, fieldName, offsetY)
-  local x = wgt.zone.x
-  local y = wgt.zone.y + offsetY
-
   local fieldinfo = getFieldInfo(fieldName)
   local fieldValue = getValue(fieldName)
+
+  if fieldinfo == nil then
+    local txt = "field info NOT available: " .. "name: " .. fieldName .. ", value: [" .. fieldValue .. "]"
+    lcd.drawText(0,0, txt, LEFT + SMLSIZE)
+    return
+  end
 
   if type(fieldValue) == "table" then
     fieldValue = "<TABLE>"
   end
 
-  local txt
-
   if fieldinfo then
-    local idUnit = -1
-    if (fieldinfo['unit']) then
-      idUnit = fieldinfo['unit']
-    end
-    --print ("aaaa unit1: " .. txtUnit)
     local txtUnit = "---"
-    if (fieldinfo.unit > 0 and fieldinfo.unit < #UNIT_ID_TO_STRING) then
-      print("idUnit: " .. fieldinfo.unit)
-      txtUnit = UNIT_ID_TO_STRING[fieldinfo.unit]
-      print("txtUnit: " .. txtUnit)
+    if (fieldinfo.unit) then
+      --log("have unit")
+      if (fieldinfo.unit > 0 and fieldinfo.unit < #UNIT_ID_TO_STRING) then
+        log("idUnit: " .. fieldinfo.unit)
+        txtUnit = UNIT_ID_TO_STRING[fieldinfo.unit]
+        log("txtUnit: " .. txtUnit)
+      end
     end
 
     local txt = fieldinfo['name'] .. "(id:" .. fieldinfo['id']
@@ -94,11 +58,10 @@ local function showField(wgt, fieldName, offsetY)
       .. txtUnit
       .. " [desc: " .. fieldinfo['desc'] .. "]"
 
-    lcd.drawText(x, y, txt, LEFT + SMLSIZE)
-
+    lcd.drawText(wgt.zone.x + 3, wgt.zone.y + offsetY, txt, LEFT + SMLSIZE)
 
     for k, v in pairs(fieldinfo) do
-      if (k ~='name' and k~='id'and k~='desc'and k~='unit') then
+      if (k ~= 'name' and k ~= 'id' and k ~= 'desc' and k ~= 'unit') then
         print("found field: " .. k)
       end
     end
@@ -109,7 +72,7 @@ local function showField(wgt, fieldName, offsetY)
   return
 end
 
-local function testPrint(wgt)
+local function refresh(wgt)
   showField(wgt, 'cell', 0)
   showField(wgt, 'Cels', 20)
   showField(wgt, 'RSSI', 40)
@@ -121,60 +84,6 @@ local function testPrint(wgt)
   showField(wgt, 'Tmr1', 150)
   showField(wgt, 'TMR1', 160)
   showField(wgt, 'timer1', 170)
-  return
 end
 
-
--- This size is for top bar widgets
-local function refreshZoneTiny(wgt)
-end
-
---- Size is 160x32 1/8th
-local function refreshZoneSmall(wgt)
-  testPrint(wgt)
-  return
-end
-
---- Size is 225x98 1/4th  (no sliders/trim)
-local function refreshZoneMedium(wgt)
-  testPrint(wgt)
-  return
-end
-
---- Size is 192x152 1/2
-local function refreshZoneLarge(wgt)
-  testPrint(wgt)
-  return
-end
-
---- Size is 460x252 1/1 (no sliders/trim/topbar)
-local function refreshZoneXLarge(wgt)
-  testPrint(wgt)
-
-  return
-end
-
--- This function allow recording of lowest cells when widget is not active
-local function background(wgt)
-  if (isWidgetInvalid(wgt)) then return end
-  return
-end
-
-local function refresh(wgt)
-
-  if (isWidgetInvalid(wgt)) then return end
-
-  if wgt.zone.w > 380 and wgt.zone.h > 165 then
-    refreshZoneXLarge(wgt)
-  elseif wgt.zone.w > 180 and wgt.zone.h > 145 then
-    refreshZoneLarge(wgt)
-  elseif wgt.zone.w > 170 and wgt.zone.h > 65 then
-    refreshZoneMedium(wgt)
-  elseif wgt.zone.w > 150 and wgt.zone.h > 28 then
-    refreshZoneSmall(wgt)
-  elseif wgt.zone.w > 65 and wgt.zone.h > 35 then
-    refreshZoneTiny(wgt)
-  end
-end
-
-return { name = "t_fieldInfo1", options = options, create = create, update = update, background = background, refresh = refresh }
+return { name = "t_fieldInfo1", options = options, create = create, update = update, refresh = refresh }
